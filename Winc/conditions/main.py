567c534055c9b915e1b199192ca7073449e33019
Barzin <barzin.development@gmail.com>
2023-03-17 15:40:49 +0100

    Add exercises: 'conditions'. (for teacher to look at problem).

---
+++ Winc/conditions/main.py
@@ -0,0 +1,75 @@
+# Do not modify these lines
+__winc_id__ = '25596924dffe436da9034d43d0af6791'
+__human_name__ = 'conditions'
+
+# Add your code after this line
+
+# def farm_action(weather, time_of_day, cow_milk_stat, loc_cows, season, slurry_tank, grass_stat):
+
+#     action = 'wait'
+
+#     if (loc_cows == 'pasture' and (time_of_day == 'night' or weather == 'rainy')):
+#         action = 'take cows to cowshed'
+#         return action
+
+#     elif (cow_milk_stat == True):
+#         if (loc_cows == 'pasture'):
+#             action = 'take cows to cowshed\nmilk cows\ntake cows back to pasture'
+#         else:
+#             action = 'milk cows'
+#         return action
+#     elif (slurry_tank == True and weather != 'sunny' and weather != 'windy'):
+#         if (loc_cows == 'pasture'):
+#             action = 'take cows to cowshed\nfertilize pasture\ntake cows back to pasture'
+#         else:
+#             action = 'fertilize pasture'
+#         return action
+#     elif (grass_stat == True and season == 'spring' and weather == 'sunny'):
+#         if (loc_cows == 'pasture'):
+#             action = 'take cows to cowshed\nmow grass\ntake cows back to pasture'
+#         else:
+#             action = 'mow grass'
+#         return action
+
+#     return action
+
+# print(farm_action('rainy', 'night', False, 'cowshed', 'winter', True, True))
+# print(farm_action('rainy', 'night', False, 'cowshed', 'winter', False, True))
+# print(farm_action('windy', 'night', True, 'cowshed', 'winter', True, True))
+# print(farm_action('sunny', 'day', True, 'pasture', 'spring', False, True))
+# ----------------------------------------------
+
+def farm_action(weather, time_of_day, cow_milking_status, location_cows, season, slurry_tank, grass_status):
+    action = "wait"
+    if location_cows == 'pasture' and (time_of_day == 'night' or weather == 'rainy'):
+        action = "take cows to cowshed"
+        # return action
+    
+    if cow_milking_status:
+        if location_cows == 'cowshed':
+            action =  "milk cows"
+        if location_cows == 'pasture':
+            action = f"take cows to cowshed\nmilk cows\ntake cows back to pasture"
+        return action   # WAAROM moet ik hier een return doen? is nergens anders nodig!
+        # print("action =>", action)
+        
+    if slurry_tank:
+        if location_cows != 'cowshed':    
+            action = "take cows to cowshed"
+        if (weather != 'sunny' and weather != 'windy' ):
+            action =  "fertilize pasture"
+        # return action
+    
+    if grass_status and (season == 'spring' and weather == 'sunny'):
+        if location_cows != 'pasture':
+            action = 'mow grass'
+        else:
+            action = 'take cows to cowshed\nmow grass\nake cows back to pasture'
+        # return action     
+     
+    return action    
+    
+print(farm_action('rainy', 'night', False, 'cowshed', 'winter', True, True))
+print(farm_action('rainy', 'night', False, 'cowshed', 'winter', False, True))
+print(farm_action('windy', 'night', True, 'cowshed', 'winter', True, True))
+print(farm_action('sunny', 'day', True, 'pasture', 'spring', False, True))
--- Winc/try.py
+++ Winc/try.py
@@ -23,7 +23,7 @@
 
 # print('this is "double quote" within single quote.')
 # print("this is 'single quote' within double quote.")
-# print("""this is escape character: \\ 
+# print("""this is escape character: \\
 #       (you can only show it whe u use two '\\\\')
 #       This way you actually escape \\ char by putting extra backslash before it.""")
 
@@ -41,4 +41,72 @@
 '''
 
 # --------------------------------------------
+"""
 
+Weather
+    rainy
+    sunny
+    windy
+    neutral
+
+Time of day
+    day
+    night
+
+Cow milking status
+    Need milking: True
+    Don't need milking: False
+
+Location of the cows
+    pasture
+    cowshed
+
+Season
+    winter
+    spring
+    summer
+    fall
+
+Slurry tank
+    Full: True
+    Not full: False
+
+Grass status
+    Long: True
+    Short: False
+
+"""
+def farm_action(weather, time_of_day, cow_milk_stat, loc_cows, season, slurry_tank, grass_stat):
+
+    action = 'wait'
+
+    if (loc_cows == 'pasture' and (time_of_day == 'night' or weather == 'rainy')):
+        action = 'take cows to cowshed'
+        # return action
+
+    elif (cow_milk_stat == True):
+        if (loc_cows == 'pasture'):
+            action = 'take cows to cowshed\nmilk cows\ntake cows back to pasture'
+        else:
+            action = 'milk cows'
+        print("ACTION ==>> ", action, "\n-----------------")
+        return action
+    elif (slurry_tank == True and weather != 'sunny' and weather != 'windy'):
+        if (loc_cows == 'pasture'):
+            action = 'take cows to cowshed\nfertilize pasture\ntake cows back to pasture'
+        else:
+            action = 'fertilize pasture'
+        # return action
+    elif (grass_stat == True and season == 'spring' and weather == 'sunny'):
+        if (loc_cows == 'pasture'):
+            action = 'take cows to cowshed\nmow grass\ntake cows back to pasture'
+        else:
+            action = 'mow grass'
+        # return action
+
+    return action
+
+print(farm_action('rainy', 'night', False, 'cowshed', 'winter', True, True))
+print(farm_action('rainy', 'night', False, 'cowshed', 'winter', False, True))
+print(farm_action('windy', 'night', True, 'cowshed', 'winter', True, True))
+print(farm_action('sunny', 'day', True, 'pasture', 'spring', False, True))
