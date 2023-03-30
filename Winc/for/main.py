2b95b7bee5f09a2a00437caa55c6c032e3656e19
Barzin <barzin.development@gmail.com>
2023-03-30 23:02:01 +0200

    Add part2 solution of the exercise: 'for'.

--- Winc/for/main.py
+++ Winc/for/main.py
@@ -8,6 +8,41 @@ __human_name__ = "for"
 
 
 """ Write your functions here. """
+def shortest_names(countries):
+    shortest_countries = [] 
+    shortest_len = len(countries[0])
+    for country in countries:
+         if len(country) < shortest_len:
+             shortest_len = len(country) 
+    
+    for country in countries:
+        if len(country) == shortest_len:
+            shortest_countries.append(country)
+            
+    return shortest_countries
+
+def most_vowels(countries):
+    vowels = ["a", "e", "i", "o", "u"]
+    counted_vowels_list = []    
+    most_vowels_list = []
+    for country in countries:
+        count_vowel = 0
+        for char in country:
+            if char.lower() in vowels:
+                count_vowel += 1
+        counted_vowels_list.append([count_vowel, country]) 
+    counted_vowels_list2 = counted_vowels_list
+    for item in counted_vowels_list:
+        for country in counted_vowels_list2:
+            if country[0] > item[0]: 
+                if country not in most_vowels_list:
+                    most_vowels_list.append(country) 
+    sorted_list = sorted(most_vowels_list, reverse=True)[:3]
+    # sorted_list = sorted(most_vowels_list, reverse=True)
+    # print(f"sorted list -> {sorted_list}")
+    return [vowel[1] for vowel in sorted_list]
+
+
 
 # This block is only run if this file is the entrypoint; python main.py
 # It is not run if it is imported as a module: `from main import *`
@@ -15,3 +50,6 @@ if __name__ == "__main__":
     countries = get_countries()
 
     """ Write the calls to your functions here. """
+    print(most_vowels(countries))
+    # most_vowels(countries)
+    
--- Winc/try.py
+++ Winc/try.py
@@ -78,3 +78,71 @@ Grass status
 """
 
 # --------------------------------------------
+
+
+""" 
+from helpers import get_countries
+
+
+
+# Leave this untouched. Wincpy uses it to match this assignment with the
+# tests it runs. 
+
+__winc_id__ = "c545bc87620d4ced81cbddb8a90b4a51"
+__human_name__ = "for"
+
+
+# Write your functions here.
+def shortest_names(countries):
+    shortest_countries = [] 
+    shortest_len = len(countries[0])
+    for country in countries:
+        if len(country) < shortest_len:
+            shortest_countries.append(country)
+    return shortest_countries
+
+
+# This block is only run if this file is the entrypoint; python main.py
+# It is not run if it is imported as a module: `from main import *`
+if __name__ == "__main__":
+    countries = get_countries()
+
+    # Write the calls to your functions here.
+# 1.
+
+print(f"len(countries) -> {len(countries)}")
+# print(shortest_names(countries))
+print(f"len(shortest_names(countries)) -> {len(shortest_names(countries))}")
+
+"""
+
+""" def most_vowels(countries):
+    vowels = ["a", "e", "i", "o", "u"]
+    counted_vowels_list = []
+    most_vowels_list = []
+    for country in countries:
+        count_vowel = 0
+        for char in country:
+            if char in vowels:
+                count_vowel += 1
+        counted_vowels_list.append([count_vowel, country]) 
+    for item in counted_vowels_list:
+        if most_vowels_list == []:
+            most_vowels_list.append(item)
+        elif  most_vowels_list != []:
+            for index, vowel_item in enumerate(most_vowels_list):
+                if item not in most_vowels_list:
+                    if item[0] > vowel_item[0]:
+                        most_vowels_list[index] = item
+    most_vowels_country = [most_vowels_list[0][1]]
+    return most_vowels_country   """
+    
+
+
+
+
+""" juist result moet zijn:
+
+['South Georgia and the South Sandwich Islands', 'United States Minor Outlying Islands', 'Micronesia, Federated States of']
+
+"""
