0355ecc58ee8541b6dc8df9ce10b17f0e3e4b917
Barzin <barzin.development@gmail.com>
2023-04-10 13:27:27 +0200

    Add Part 2: 'Add Stamp' to exercise: 'dictionaries'.

--- Winc/dictionariesv2/main.py
+++ Winc/dictionariesv2/main.py
@@ -15,7 +15,29 @@ def create_passport(name, date_of_birth, place_of_birth, height, nationality):
     }
     return passport
 
+def add_stamp(passport, country):
+    if 'stamps' in passport:
+        print(f"stamps exists already in passport!")
+        if country not in passport['stamps']:
+            print(f"But '{country}' doesn't yet exist as a stamp in the 'stamps'.\nAdding {country}....")
+            passport['stamps'].append(country)
+            print(f"{country} stamp added to passport!\n{'--'*30}")
+            print(f"Now this is the new passport 'stamps' values: {passport['stamps']}")
+        else:
+            print(f"{country} stamp already exists in passport stamps!")
+    else:
+        print(f"Current passport items -> {passport}\n{'--'*30}")
+        print("creating 'stamps' key in passport...")
+        passport['stamps'] = [country]
+        print(f"{country} stamp added to passport!\n{'--'*30}")
+        print(f"Passport items after adding the 'stamps' key and value -> {passport}")
+    return passport
+    
+
+
 if __name__ == "__main__":
     countries = get_countries()
-    myPassport = create_passport("barzin", "1982-08-29","Teheran", 1.70, "Netherlands")
-    print(myPassport)
+    myPassport = create_passport("Glenn", "1990-04-19","Frankfurt", 1.80, "Germany")
+    add_stamp(myPassport, 'France')
+
+    
