1204f5bdaded7e4db0b6fda572912e0850bead14
Barzin <barzin.development@gmail.com>
2023-04-21 11:23:30 +0200

    Add Part 2: 'Force' of exercise: 'Arguments'.

--- Winc/arguments/main.py
+++ Winc/arguments/main.py
@@ -4,7 +4,7 @@ __human_name__ = 'arguments'
 
 # Add your code after this line
 
-# part1:
+# part1: Greeting
 
 def greet(name, greeting="Hello, <name>!"):
     if '<name>' in greeting:
@@ -15,3 +15,29 @@ def greet(name, greeting="Hello, <name>!"):
 
 print(greet('barry'))
 print(greet('barry', 'salam'))
+
+
+
+# Part2: Force
+
+bodies = {
+    'sun': 274,
+    'jupiter': 24.9,
+    'neptune': 11.2,
+    'saturn': 10.4,
+    'earth': 9.8,
+    'uranus': 8.9,
+    'venus': 8.9,
+    'mars': 3.7,
+    'mercury': 3.7,
+    'moon': 1.6,
+    'pluto': 0.6,
+}
+
+def force(mass, body='earth'):
+    return round(mass * bodies[body], 1)
+
+print(force(2,'moon'))
+
+
+
