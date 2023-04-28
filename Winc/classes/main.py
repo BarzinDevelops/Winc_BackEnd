552e2848e4b22637b400651d152e35c25778765d
Barzin <barzin.development@gmail.com>
2023-04-28 14:04:07 +0200

    Define an instance method strength that takes no arguments and returns a tuple like the following, where the string speed is replaced by the player's actual highest attribute and the value corresponds to that attribute:('speed', 0.8). If multiple attributes share the same value, prioritize as follows:speed > endurance > accuracy.

--- Winc/classes/main.py
+++ Winc/classes/main.py
@@ -19,20 +19,27 @@ class Player:
         self.speed = speed
         self.endurance = endurance
         self.accuracy = accuracy
+    
     def introduce(self):
         return f'Hello everyone, my name is {self.name}.'
-        # if self.name:
-        #     return f'Hello everyone, my name is {self.name}.'
-        # else:
-        #     return f'Hello everyone, my name is Super Bob.'
+
+    def strength(self):
+        if self.speed >= self.endurance and self.speed >= self.accuracy:
+            return ('speed', self.speed)
+        elif self.endurance >= self.accuracy:
+            return ('endurance', self.endurance)
+        else:
+            return ('accuracy', self.accuracy)
 
 
 if __name__ == '__main__':
-    pl1 = Player('player1',1, 0, 0,)
-    # pl1 = Player('',1, 0, 0)
+    # pl1 = Player('player1',1, 0, 0,)
+    pl1 = Player('pl1',0.7, 0.9, 0.9)
 
-    print(f"pl1.introduce -> {pl1.introduce()}")
+    # print(f"pl1.introduce -> {pl1.introduce()}")
     # pl2 = Player('pl2', 0, -1, 0)
     # pl3 = Player('pl3', 0, 0, -1)
     # pl4 = Player('pl4', -1, 0, 0)
+    
+    print(pl1.strength())
     
