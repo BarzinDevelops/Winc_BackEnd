04b9c4abb4386d3cf3f120dfde91c326c7c4d5e2
Barzin <barzin.development@gmail.com>
2023-04-28 12:12:59 +0200

    Write class 'Players' and define initialization method: __init__ with parameters: (name, speed, endurance and accuracy). Also raise VlaueError if each argument is not between 0 and 1(inclusive).

--- Winc/classes/main.py
+++ Winc/classes/main.py
@@ -3,3 +3,23 @@ __winc_id__ = '04da020dedb24d42adf41382a231b1ed'
 __human_name__ = 'classes'
 
 # Add your code after this line
+class Player:
+    def __init__(self, name, speed, endurance, accuracy):
+        if not 0 <= speed <= 1:
+            raise ValueError('Speed must be between 0 and 1')
+        if not 0 <= endurance <= 1:
+            raise ValueError('endurance must be between 0 and 1')
+        if not 0 <= accuracy <= 1:
+            raise ValueError('accuracy must be between 0 and 1')
+        
+        self.name = name
+        self.speed = speed
+        self.endurance = endurance
+        self.accuracy = accuracy
+
+
+if __name__ == '__main__':
+    # pl1 = Player('pl1', -1, 0, 0)
+    # pl2 = Player('pl2', 0, -1, 0)
+    pl3 = Player('pl3', 0, 0, -1)
+    # pl4 = Player('pl4', -1, 0, 0)
