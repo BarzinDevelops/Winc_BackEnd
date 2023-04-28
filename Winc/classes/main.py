07e476f3ccc77d579f01c7652f40ab6889a8ebe1
Barzin <barzin.development@gmail.com>
2023-04-28 14:12:50 +0200

    Create new class 'Commentator'. Implement it in such a way that we can instantiate it with a name and when printed (ray.name) it returns the name we gave when instantiated the Commentator class.

--- Winc/classes/main.py
+++ Winc/classes/main.py
@@ -3,6 +3,7 @@ __winc_id__ = '04da020dedb24d42adf41382a231b1ed'
 __human_name__ = 'classes'
 
 # Add your code after this line
+# Part 1: Players
 class Player:
     def __init__(self, name, speed, endurance, accuracy):
         if not 0 <= speed <= 1:
@@ -31,15 +32,14 @@ class Player:
         else:
             return ('accuracy', self.accuracy)
 
+# Part 2: Commentators
+class Commentator:
+    def __init__(self, name):
+        self.name = name
+        
 
 if __name__ == '__main__':
-    # pl1 = Player('player1',1, 0, 0,)
-    pl1 = Player('pl1',0.7, 0.9, 0.9)
-
-    # print(f"pl1.introduce -> {pl1.introduce()}")
-    # pl2 = Player('pl2', 0, -1, 0)
-    # pl3 = Player('pl3', 0, 0, -1)
-    # pl4 = Player('pl4', -1, 0, 0)
-    
-    print(pl1.strength())
+    # pl1 = Player('pl1',0.7, 0.9, 0.9)
+    ray = Commentator('Ray Hudson')
+    print(ray.name)
     
