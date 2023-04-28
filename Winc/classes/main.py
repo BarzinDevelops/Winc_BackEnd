9e1a2aec370e4fa14de6b0a3a737cb0781275210
Barzin <barzin.development@gmail.com>
2023-04-28 13:55:06 +0200

    Add 'introduce method' which returns string: 'Hello everyone, my name is Bob'. If name is given when Player is instantiated, then that will be used instead of 'Bob'.

--- Winc/classes/main.py
+++ Winc/classes/main.py
@@ -12,14 +12,27 @@ class Player:
         if not 0 <= accuracy <= 1:
             raise ValueError('accuracy must be between 0 and 1')
         
-        self.name = name
+        if name:
+            self.name = name
+        else:
+            self.name = 'Bob'
         self.speed = speed
         self.endurance = endurance
         self.accuracy = accuracy
+    def introduce(self):
+        return f'Hello everyone, my name is {self.name}.'
+        # if self.name:
+        #     return f'Hello everyone, my name is {self.name}.'
+        # else:
+        #     return f'Hello everyone, my name is Super Bob.'
 
 
 if __name__ == '__main__':
-    # pl1 = Player('pl1', -1, 0, 0)
+    pl1 = Player('player1',1, 0, 0,)
+    # pl1 = Player('',1, 0, 0)
+
+    print(f"pl1.introduce -> {pl1.introduce()}")
     # pl2 = Player('pl2', 0, -1, 0)
-    pl3 = Player('pl3', 0, 0, -1)
+    # pl3 = Player('pl3', 0, 0, -1)
     # pl4 = Player('pl4', -1, 0, 0)
+    
