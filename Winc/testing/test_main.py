5de465710fcddc118948536068a9ed12a93fd396
Barzin <barzin.development@gmail.com>
2023-05-06 14:15:26 +0200

    Add Part2: implementation of tescases for 'flatten_dict()'. 1.testing if function returns type list. 2. testing if returned list contains values of given dict. 3. testing if returned list, also can contains a list of values (i.e. {'a': [42, 350], 'b': 3.14} ).

--- Winc/testing/test_main.py
+++ Winc/testing/test_main.py
@@ -4,6 +4,19 @@ import pytest, main
 def test_get_none():
     assert main.get_none() == None
 
+# Part2:
 
+# check if this returns a list when given a dict:
+def test_flatten_dict():
+    assert main.flatten_dict({}) is list
+
+
+# check if it returns list with values of the given dict:
+def test_flatten_dict_with_values():
+    assert main.flatten_dict({'a': 42, 'b': 3.14})
+    
+# check if returned, also contains list of values within returned list:
+def test_flatten_dict_with_list_as_item():
+    assert main.flatten_dict({'a': [42, 350], 'b': 3.14})
 
 
