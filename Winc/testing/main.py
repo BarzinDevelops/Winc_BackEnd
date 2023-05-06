9339029d301f6ee72134cc7b4dd92b8651d88abb
Barzin <barzin.development@gmail.com>
2023-05-06 14:09:37 +0200

    Add Part1: implementation of test_get_none() and function get_none() for exercise: 'testing'.

---
+++ Winc/testing/main.py
@@ -0,0 +1,7 @@
+def get_none():
+    return None
+
+
+
+if __name__ == '__main__':
+    print(get_none())
---
+++ Winc/testing/test_main.py
@@ -0,0 +1,9 @@
+import pytest, main
+
+# Part1: 
+def test_get_none():
+    assert main.get_none() == None
+
+
+
+
