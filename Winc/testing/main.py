0edc666a1709c0235efb61bce9714516503add66
Barzin <barzin.development@gmail.com>
2023-05-12 17:15:56 +0200

    Refactor function 'flatten_dict' so it matches test that only check for one level deep in each given dict. Also add usefull tips to handy_commands.txt file.

--- Winc/testing/main.py
+++ Winc/testing/main.py
@@ -2,14 +2,30 @@
 def get_none():
     return None
 
+# breakpoint()
 # Part2_2: implement the method: flatten_dict()
-def flatten_dict(received_dict):
-    result_list  = list(received_dict.values())
-    return result_list
+""" def flatten_dict(received_dict):
+    result_list  = []
+    for item in received_dict:
+        print(f"item : {item}")
+        if type(item) is dict or type(item) is list:
+             result_list.extend(flatten_dict(item)) 
+        else:
+            print(f"item is: {item}")
+            result_list.append(item)
+            print(f"result_list is na deze append: {result_list}")
+    return result_list """
+    
+    
+def flatten_dict(dict_arg):
+  dict_list = [] 
+  for k,v in dict_arg.items():
+    dict_list.append(v)
+  return dict_list
 
 
 if __name__ == '__main__':
-    # print(get_none())
-    # print(f"is of type list? -> {type(flatten_dict({})) is list}")
-    # print(f"returns values of dict? -> {flatten_dict({'a': 42, 'b': 3.14})}")
-    # print(f"has [values] as part of dict items? -> {flatten_dict({'a': [42, 350], 'b': 3.14})}")
+    # print('returned from first call: ',flatten_dict({'a': [42, 350], 'b': 3.14}))
+    # print('-'*30)
+    # print('returned from second call: ',flatten_dict({'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14}))
+    pass
--- Winc/testing/test_main.py
+++ Winc/testing/test_main.py
@@ -8,23 +8,25 @@ def test_get_none():
 
 # check if the value returned by flatten_dict is a list at all:
 def test_flatten_dict():
-    print(f"Result of test1: {type(main.flatten_dict({})) is list}")
+    # print(f"test1-> is it a list?: {type(main.flatten_dict({})) is list}")
     assert type(main.flatten_dict({})) is list
+    # print(f"test1-> is it empty list?: {main.flatten_dict({})}")
+    assert main.flatten_dict({}) == []
 
 
 # check if it returns list with values of the given dict:
 def test_flatten_dict_with_values():
     # om je eigen print ongeacht passed/failed te kunnen zien: 
-    # geef volgende commando in terminal ->  pytest -rP
-    print(f"Result of test2: {main.flatten_dict({'a': 42, 'b': 3.14})}")
-    assert main.flatten_dict({'a': 42, 'b': 3.14})
+    # geef volgende commando in terminal ->  pytest -rP -v
+    # print(f"Result of test2: {main.flatten_dict({'a': 42, 'b': 3.14})}")
+    assert main.flatten_dict({'a': 42, 'b': 3.14}) == [42, 3.14]
     
 # check if returned, also contains list of values within returned list:
 def test_flatten_dict_with_list_as_item():
-    print(f"Result of test3: {main.flatten_dict({'a': [42, 350], 'b': 3.14})}")
-    assert main.flatten_dict({'a': [42, 350], 'b': 3.14})
+    # print(f"Result of test3: {main.flatten_dict({'a': [42, 350], 'b': 3.14})}")
+    assert main.flatten_dict({'a': [42, 350], 'b': 3.14}) == [[42, 350], 3.14]
 
-# --------------------------------------
+""" # --------------------------------------
         # Part2_challange
 #---------------------------------------
 # implement flatten_dict in such a way that 
@@ -37,7 +39,12 @@ def test_flatten_dict_with_list_as_item():
     # [42]
 
 # To do this you may want to look into a 
-# technique that is called recursion.
+# technique that is called recursion. """
 
-
+def test_flatten_dict_challenge1():
+    assert main.flatten_dict_challenge({'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14}) == [42, 350, 3.14]
+    
+    
+def test_flatten_dict_challenge2():
+    assert main.flatten_dict_challenge({'a': [{'inner_inner_a': 42}]}) == [42]
 
